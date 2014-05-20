6d5b53a5ec811d511db8975bbc4777b1c7de0802
Paul Emile Gerard <paul1gerard@gmail.com>
2014-05-19 22:26:32 -0500

    after code review

--- client/admin/editPhoto.js
+++ client/admin/editPhoto.js
@@ -1,30 +1,29 @@
-var chosenId;//Id from dropdown exists regardless of session
-             //unlike dropdown
 
 Template.editPhoto.photos = function(){
 	return Photos.find({});
+}
+Template.editPhoto.editPhotoId = function(){
+	return Photos.findOne({_id: Session.get("editView")})
 }
 
 Template.editPhoto.events({
  "click [data-action='updatePhoto']" : function(e, t){
  	e.preventDefault();
- 	Photos.update({_id: chosenId},
+ 	Photos.update({_id: Session.get("editView")},
  	{$set: {
- 	title: $(t.find("[name='titleEdit']")).val(), 
+ 		title: $(t.find("[name='titleEdit']")).val(), 
   	description: $(t.find("[name='descriptionEdit']")).val()
-    }      })
-    Session.set("editView", false); 
+   }      })
+  Session.set("editView", false); 
    
  },
  "click [data-action='selectPhoto']" : function(e, t){
  	e.preventDefault();
- 	chosenId = $(t.find("[name='photoDropdown']")).val();
- 	Session.set("editView", true);
+ 	Session.set("editView", $(t.find("[name='photoDropdown']")).val());
  },
 
  "click [data-action='cancel']" : function(e, t){
  	e.preventDefault();
  	Session.set("editView", false);  
  }
-
 });
