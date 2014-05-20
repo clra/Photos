be6af2dccac4dfa9e341ea83d6fc49bbaa4d9e17
Paul Emile Gerard <paul1gerard@gmail.com>
2014-05-19 21:09:19 -0500

    Sessions working on edit page

--- client/admin/editPhoto.js
+++ client/admin/editPhoto.js
@@ -1,3 +1,6 @@
+var chosenId;//Id from dropdown exists regardless of session
+             //unlike dropdown
+
 Template.editPhoto.photos = function(){
 	return Photos.find({});
 }
@@ -5,22 +8,23 @@ Template.editPhoto.photos = function(){
 Template.editPhoto.events({
  "click [data-action='updatePhoto']" : function(e, t){
  	e.preventDefault();
-  console.log("you submited the form.");
-  
- 
- 	Photos.update({_id: $("[name='photoDropdown']").val()},
+ 	Photos.update({_id: chosenId},
  	{$set: {
  	title: $(t.find("[name='titleEdit']")).val(), 
   	description: $(t.find("[name='descriptionEdit']")).val()
-   }})
+    }      })
+    Session.set("editView", false); 
    
+ },
+ "click [data-action='selectPhoto']" : function(e, t){
+ 	e.preventDefault();
+ 	chosenId = $(t.find("[name='photoDropdown']")).val();
+ 	Session.set("editView", true);
+ },
+
+ "click [data-action='cancel']" : function(e, t){
+ 	e.preventDefault();
+ 	Session.set("editView", false);  
  }
 
 });
-/*
-Photos.update({_id: $("[name='photoDropdown']").val()},
- 	{$set: {
- 	title: $("[name='titleEdit']").val(), 
-  	description: $("[name='descriptionEdit']").val()
-   }}});
-   */
